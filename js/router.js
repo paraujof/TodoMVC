dc9789d4dc4f98fe1da690dbc23d7693ce7e28d7
Pablo <ferarap@gmail.com>
2014-03-06 20:17:52 +0000

    Filtering incomplete todos

--- js/router.js
+++ js/router.js
@@ -1,6 +1,6 @@
 Todos.Router.map(function() {
   this.resource('todos', { path: '/'  }, function () {
-
+  	this.route('active');
   });
 });
 
@@ -13,5 +13,16 @@ Todos.TodosRoute = Ember.Route.extend({
 Todos.TodosIndexRoute = Ember.Route.extend({
   model: function() {
     return this.modelFor('todos');
+  }
+});
+
+Todos.TodosActiveRoute = Ember.Route.extend({
+  model: function(){
+    return this.store.filter('todo', function(todo) {
+      return !todo.get('isCompleted');
+    });
+  },
+  renderTemplate: function(controller) {
+    this.render('todos/index', {controller: controller});
   }
 });
